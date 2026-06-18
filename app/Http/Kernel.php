ccea9d6b74a2134b93d4decaa667b42884a8f426
bsms66366 <cj.taylor@bsms.ac.uk>
2026-06-18 11:01:46 +0100

    Stutalk implementation

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,6 +23,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\CorsMiddleware::class,
+          \App\Http\Middleware\LogRequestId::class,
     ];
 
     /**
